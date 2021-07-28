3b387cde7da58b0e0b0f4b73bcec3f8dc5f6cc28
Connor Marble <connor.marble@gmail.com>
2021-07-28 11:39:50 -0400

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import Main from './ui/layout/Main';
-import Navbar from './ui/layout/Navbar';
+import React from "react";
+import Main from "./ui/layout/Main";
+import Navbar from "./ui/layout/Navbar";
 
 const App: React.FC = () => {
-    return (
-        <div className="App">
-            <Navbar />
-            <Main />
-        </div>
-    );
+  return (
+    <div className="App">
+      <Navbar />
+      <Main />
+    </div>
+  );
 };
 
 export default App;
